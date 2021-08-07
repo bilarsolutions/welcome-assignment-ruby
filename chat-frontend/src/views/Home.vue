023823243d70b6d51c9192dbf6772a149cf71cc6
Bilar Solutions <bilarsolutions2021@gmail.com>
2021-08-07 20:26:00 +0500

    fix: Add CORS

--- chat-frontend/src/views/Home.vue
+++ chat-frontend/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Chat :messages="messages"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Chat from "@/components/Chat.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Chat,
   },
 };
 </script>
